136ad98d792bb90649277b23d3600b940ed5b2ca
Philipp Kurz <kurz_philipp@gmx.de>
2021-03-09 16:42:50 +0000

    Added table create queries

--- sql_queries.py
+++ sql_queries.py
@@ -8,20 +8,56 @@ time_table_drop = ""
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (
+                                songplay_id int PRIMARY KEY,
+                                start_time timestamp with time zone NOT NULL,
+                                user_id int NOT NULL,
+                                level text,
+                                song_id text NOT NULL,
+                                artist_id text NOT NULL,
+                                session_id text NOT NULL,
+                                location text,
+                                user_agent text                         
+                                )
+                            """)
 
-user_table_create = ("""
-""")
+user_table_create = (""" CREATE TABLE IF NOT EXSITS users (
+                            user_id int PRIMARY KEY,
+                            first_name text NOT NULL,
+                            last_name text NOT NULL,
+                            gender text,
+                            level text               
+                            )
+                        """)
 
-song_table_create = ("""
-""")
+song_table_create = (""" CREATE TABLE IF NOT EXISTS songs (
+                            song_id text PRIMARY KEY,
+                            title text NOT NULL,
+                            artist_id text NOT NULL,
+                            year int,
+                            duration numeric                            
+                            )
+                        """)
 
-artist_table_create = ("""
-""")
+artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (
+                            artist_id text PRIMARY KEY,
+                            name text NOT NULL,
+                            location text,
+                            latitude numeric,
+                            longitude numeric
+                            )
+                        """)
 
-time_table_create = ("""
-""")
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time (
+                        start_time timestamp with timezone,
+                        hour int,
+                        day int,
+                        week int,
+                        month int,
+                        year int,
+                        weekday text
+                        )
+                    """)
 
 # INSERT RECORDS
 
