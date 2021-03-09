a4a973c2da88698ca3870df8fb513048d16ee7fb
Philipp Kurz <kurz_philipp@gmx.de>
2021-03-09 16:46:37 +0000

    Added table drop queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS times"
 
 # CREATE TABLES
 
@@ -48,7 +48,7 @@ artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (
                             )
                         """)
 
-time_table_create = (""" CREATE TABLE IF NOT EXISTS time (
+time_table_create = (""" CREATE TABLE IF NOT EXISTS times (
                         start_time timestamp with timezone,
                         hour int,
                         day int,
